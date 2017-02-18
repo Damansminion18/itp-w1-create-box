7d833f38cd715601c8558ffea2ebd3dac91b07f6
Jared Letteer <jaredletteer15@gmail.com>
2017-02-18 17:15:40 +0000

    Initialcommit

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,32 @@
 
 
 def create_box(height, width, character):
-    pass
+    box = ''
+    for i in range(height):
+        box_width = ''
+        for j in range(width):
+            box_width += character
+        box_width += '\n'
+        box += box_width
+    return box
 
+"""
+def create_special_box(height, width, character):
+    topbot = width*character + '\n'
+    middle = ''
+    for i in range(height-2):
+        box_width = character
+        for j in range(width-2):
+            box_width += ' '
+        box_width += character + '\n'
+        middle += box_width
+    
+    finalbox = topbot + middle + topbot
+    
+    return finalbox 
+"""    
 
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+    print(create_box(3, 4, '*'))
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,13 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+"""
+special_box_expected = """
+XXXX
+X  X
+XXXX
+""".lstrip()
+"""
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -25,5 +32,13 @@ class TestCreateBox(unittest.TestCase):
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+        
+    def test_large_box(self):
+        self.assertEqual(create_box(3,24,'x'), third_box_expected)
 
+#    def test_special_box(self):
+#        self.assertEqual(create_box(3,4,'X'), special_box_expected)
+    
+    
     # Add your own test using third_box_expected
+    
